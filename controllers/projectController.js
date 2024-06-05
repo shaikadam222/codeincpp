a62844e4dc2405296c52e28adf397cdbfa886702
Shaik <shaikadam642@gmail.com>
2024-06-05 18:34:39 +0530

    CRUD operations of PROJECT done

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -18,4 +18,49 @@ async function createproject(req,res) {
     }
 }
 
-module.exports = {createproject};
+async function getproject(req,res) {
+    const {projectName} = req.body;
+    try {
+        const pname = await Project.findOne({projectName});
+        if(!pname) {
+            return res.send("There's no such project").status(401);
+        } else {
+            return res.status(200).json({files : pname.files});
+        }
+    }catch(err) {
+        console.log(err);
+        res.send('Error while fetching projects').status(500);
+    }
+}
+
+async function updateproject(req,res) {
+    var {projectName,updatedName} = req.body;
+    try {
+        const projectid = await Project.findOne({projectName});
+        if(!projectid) {    
+            return res.send('No such project exists').staus(403);
+        }
+        projectName = updatedName;
+        const pname = await Project.findByIdAndUpdate(projectid._id,{projectName},{new : true});
+        return res.status(201).json({pname});
+    }catch(err) {
+        console.log(err);
+        res.send('Error while updating project').status(500);
+    }
+}
+
+async function deleteproject(req,res) {
+    const {projectName} = req.body;
+    try {
+        const projectid = await Project.findOne({projectName});
+        if(!projectid) {    
+            return res.send('No such project exists').staus(403);
+        }
+        const pname = await Project.findByIdAndDelete(projectid._id);
+        return res.status(201).send('Successfully deleted');
+    }catch(err) {
+        console.log(err);
+        res.send('Error while deleteing project').status(500);
+    }
+}
+module.exports = {createproject,getproject,updateproject,deleteproject};
--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
 const { signUp, login } = require('./routes/userRoutes');
 const { authenticate } = require('./middleware/authenticate');
-const { createproject } = require('./controllers/projectController');
+const { createproject,getproject,updateproject,deleteproject } = require('./controllers/projectController');
 
 function initRoutes(app) {
     app.post('/signup', signUp);
     app.post('/login', login);
     app.post('/',authenticate,createproject);
+    app.get('/',authenticate,getproject);
+    app.put('/',authenticate,updateproject);
+    app.delete('/',authenticate,deleteproject);
 }
 
 module.exports = { initRoutes };
